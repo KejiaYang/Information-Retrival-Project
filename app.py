bdf54e619e54a11e990b2cea8789dc9276ec9ea2
KejiaYang <ykejia@umich.edu>
2018-12-03 21:18:47 -0500

    get query string in python file from front-end

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 from random import randint
  
 app = Flask(__name__)
- 
- 
+
 #@app.route("/hello/<string:name>")
-@app.route("/hello/")
+@app.route("/hello",methods=['GET', 'POST'])
 def hello():
     quotes = [ "'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ",
                "'Computer science is no more about computers than astronomy is about telescopes' --  Edsger Dijkstra ",
@@ -14,10 +13,17 @@ def hello():
                "'Mathematics is the key and door to the sciences.' -- Galileo Galilei",
                "'Not everyone will understand your journey. Thats fine. Its not their journey to make sense of. Its yours.' -- Unknown"  ]
     randomNumber = randint(0,len(quotes)-1) 
-    quote = quotes[randomNumber] 
- 
+    quote = quotes[randomNumber]
+    qr = "NO QUERY"
+    if request.method == 'POST':
+      print ("ifffffff")
+      qr = request.get_json(force=True)
+      print(qr)
+
     return render_template(
         'idx.html',**locals())
+
+
  
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='127.0.0.1', port=5000)
