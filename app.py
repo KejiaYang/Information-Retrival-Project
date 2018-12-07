29561b873e3f4ad8aa38ff29ae00cdc351c9db90
KejiaYang <ykejia@umich.edu>
2018-12-07 15:13:36 -0500

    put all info into UI

--- app.py
+++ app.py
@@ -43,6 +43,41 @@ def rt():
 def about():
     return render_template(
         'about.html',**locals())
+
+@app.route("/hello/methodology.html",methods=['GET', 'POST'])
+def methodology():
+    return render_template(
+        'methodology.html',**locals())
+
+@app.route("/hello/dataset.html",methods=['GET', 'POST'])
+def dataset():
+    return render_template(
+        'dataset.html',**locals())
+
+@app.route("/hello/bm25.html",methods=['GET', 'POST'])
+def bm25():
+    return render_template(
+        'bm25.html',**locals())
+
+@app.route("/hello/bm25ctf.html",methods=['GET', 'POST'])
+def bm25ctf():
+    return render_template(
+        'bm25ctf.html',**locals())
+
+@app.route("/hello/teammember.html",methods=['GET', 'POST'])
+def teammember():
+    return render_template(
+        'teammember.html',**locals())
+
+@app.route("/hello/reference.html",methods=['GET', 'POST'])
+def reference():
+    return render_template(
+        'reference.html',**locals())
+
+@app.route("/hello/performance.html",methods=['GET', 'POST'])
+def performance():
+    return render_template(
+        'performance.html',**locals())
  
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
